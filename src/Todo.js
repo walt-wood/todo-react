97dbc3d79ae28b645b102877d52fb192657ef3c2
wwood44 <80364037+wwood44@users.noreply.github.com>
2022-03-18 12:46:05 -0500

    Removed comments inside rendered(returned)

--- src/Todo.js
+++ src/Todo.js
@@ -1,17 +1,17 @@
 function Todo(props) {
     // This approach would work in JavaScript, but it is an imperitive approach. For React, we use the declarative approach below, which describes the target result.
     // document.querySelector('button').addEventListener('click');
+    /* Can add onClick attribute b/c these HTML tags are really React components, and they are case sensitive. 
+        Note: Do not use regular function call (deleteHandler()) with parenthesis, b/c the function would be executed when the button element is rendered.
+        We use (deleteHandler) w/o parenthesis to point to the function above.
+        Must use className instead of class, b/c this is JSX.*/
     function deleteHandler() {
         console.log("Clicked!");
     }
     return (
     <div className="card">
-        <h2>{props.text}</h2>
-        {/* Must use className instead of class, b/c this is JSX.*/}
-        <div className="actions">
-            {/* Can add onClick attribute b/c these HTML tags are really React components, and they are case sensitive. 
-                Note: Do not use regular function call (deleteHandler()) with parenthesis, b/c the function would be executed when the button element is rendered.
-                We use (deleteHandler) w/o parenthesis to point to the function above.*/}
+        <h2>{props.text}</h2>        
+        <div className="actions">            
             <button className="btn" onClick={deleteHandler}>Delete</button>
         </div>
     </div>
