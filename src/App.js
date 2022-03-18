d8017e10884895c66e7ccfe6bc45c9e493342c2a
wwood44 <80364037+wwood44@users.noreply.github.com>
2022-03-18 13:01:49 -0500

    Added Backdrop.js component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import Todo from './components/Todo';
+import Backdrop from "./components/Backdrop";
+import Todo from "./components/Todo";
+import Modal from "./components/Modal";
 
 function App() {
-  return ( 
-      <div>
-        <h1>My Todos</h1>
-        <Todo text="Learn React"/>
-        <Todo text="Master React"/>
-        <Todo text="Explore the full React Course"/>
-      </div>    
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="Learn React" />
+      <Todo text="Master React" />
+      <Todo text="Explore the full React Course" />
+      <Modal />
+      <Backdrop />
+    </div>
   );
 }
 
---
+++ src/components/Backdrop.js
@@ -0,0 +1,7 @@
+// Provide gray backdrop when Modal is open
+// The div has no content, so can be written as self-closing.
+function Backdrop() {
+  return <div className="backdrop" />;
+}
+
+export default Backdrop;
